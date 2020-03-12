dd0d0aeaeca3236c9fe7cff2df8ddb74fab4dd35
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2020-03-11 22:23:20 -0400

    DEbugging

--- server/main/app.js
+++ server/main/app.js
@@ -3,16 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var indexRouter = require('./routes')
+
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', indexRouter)
 
 module.exports = app;

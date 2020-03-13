a4b147c933088df42f86d9868e422dc0ab0ce3b7
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2020-03-12 21:24:42 -0400

    Added the new working files. No dependenciy issues.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import ContextState from './context_state_config';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-    return(
-      <div>
-      React
-      <ContextState />
-      </div>)
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
